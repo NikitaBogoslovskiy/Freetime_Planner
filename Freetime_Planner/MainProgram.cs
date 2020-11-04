f0db59cbb51134739fcf50791894a30c6987ff45
Nikita <nikita-beeline@yandex.ru>
2020-11-04 22:15:12 +0300

    Отправка первых файлов

---
+++ Freetime_Planner/MainProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace Freetime_Planner
+{
+    class MainProgram
+    {
+        public static bool Works = true;
+        static void Main(string[] args)
+        {
+            if (Bot.Authorize())
+            {
+                while (Works)
+                {
+                    switch (ReadLine().ToLower())
+                    {
+                        case "exit": Environment.Exit(0); break;
+                        default: break;
+                    }
+                }
+            }
+            WriteLine("Нажмите ENTER чтобы выйти...");
+            ReadLine();
+        }
+    }
+}
