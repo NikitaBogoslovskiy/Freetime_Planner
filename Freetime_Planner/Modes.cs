cc520901c36a5bebe134b1d226b5434b8b0d4c6c
Nikita <nikita-beeline@yandex.ru>
2020-11-10 21:54:19 +0300

    Добавление User, Users, Modes и комментариев

---
+++ Freetime_Planner/Modes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Freetime_Planner
+{
+    public static class Modes
+    {
+        public enum Mode { Default, Film, TV, Food, Search, Genres, Recommendations, PlanToWatch, Random, Soundtrack, Back } 
+        public static Mode ConvertIntoMode(string message)
+        {
+            switch (message)
+            {
+                case "Фильмы": return Mode.Film;
+                case "Сериалы": return Mode.TV;
+                case "Еда под просмотр": return Mode.Food;
+                case "По названию": return Mode.Search;
+                case "По жанру": return Mode.Genres;
+                case "Рекомендации": return Mode.Recommendations;
+                case "Планирую смотреть": return Mode.PlanToWatch;
+                case "Рандом": return Mode.Random;
+                case "Саундтрек": return Mode.Soundtrack;
+                case "Назад": return Mode.Back;
+                default: return Mode.Default;
+            }
+        }
+    }
+}
---
+++ Freetime_Planner/Users.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Freetime_Planner.User;
+using System.IO;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace Freetime_Planner
+{
+    public static class Users
+    {
+        /// <summary>
+        /// Путь к json-файлу со списком пользователей
+        /// </summary>
+        private static string users_path = @"Users_Dict\Users_Dict.json";
+
+        /// <summary>
+        /// Словарь пользователей, где ключ - это ID, а значение - объект класса User
+        /// </summary>
+        public static Dictionary<long, User> Users_Dict { get; set; }
+
+        /// <summary>
+        /// Загружает json-файл со списком известных на данный момент пользователей по пути, указанному в поле users_path
+        /// </summary>
+        public static void Upload()
+        {
+            var dict = JsonConvert.DeserializeObject<Dictionary<long, User>>(File.ReadAllText(users_path));
+            Users_Dict = dict ?? new Dictionary<long, User>();
+        }
+
+        /// <summary>
+        /// Выгружает актуальный список пользователей в json-файл по пути, который указан в поле users_path
+        /// </summary>
+        public static void Unload()
+        {
+            File.WriteAllText(users_path, JsonConvert.SerializeObject(Users_Dict));
+        }
+
+        /// <summary>
+        /// Получение объекта нашего класса User из объекта класса User пространства имен VkNet
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <returns></returns>
+        public static User GetUser(VkNet.Model.User sender)
+        {
+            if (Users_Dict.Select(user => user.Key).Contains(sender.Id))
+                return Users_Dict[sender.Id];
+            else
+            {
+                var new_user = new User(sender.FirstName, sender.LastName, sender.Id);
+                Users_Dict.Add(sender.Id, new_user);
+                Unload();
+                return new_user;
+            }
+        }
+    }
+}
